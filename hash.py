0d3a97640ce241d4c8479ca305d9e4ec277a73e2
Ahmed Ihab <ahmedinouh@gmail.com>
2024-06-24 17:19:27 +0300

    implemetin hash

---
+++ hash.py
@@ -0,0 +1,12 @@
+# hash_map = {}
+# hash_map['name'] = 'Ahmed'
+# hash_map['age'] = 20
+# hash_map['city'] = 'Mansoura'
+
+# print(hash_map['name'])
+# print(hash_map['age'])
+# print(hash_map['city'])
+
+hash_map = {}
+
+
